4a5b8e762acf26484dd600e713b661df9ba47a73
balaji0206-uff <balajikalaivani0206@gmail.com>
2025-08-27 23:51:34 -0700

    Add files via upload

---
+++ mark sheet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+    printf("\t\t\t\t\t\t\+BCA-A");
+    printf("\n______________________________________________________");
+    printf("\nname:ravi\t\t\t\t\t\t\treg no:001");
+    printf("\n______________________________________________________");
+    float mark[5]={78,81,53,64,51};
+    float total=mark[0]+mark[1]+mark[2]+mark[3]+mark[4];
+    float average=total/5;
+    printf("ntamil\t\t\t\t\t:%5.2f",mark[0]);
+    printf("nenglish\t\t\t\t\t:%5.2f",mark[1]);
+    printf("nmaths\t\t\t\t\t:%5.2f",mark[2]);
+    printf("nscience\t\t\t\t\t:%5.2f",mark[3]);
+    printf("nsocial\t\t\t\t\t:%5.2f",mark[4]);
+    printf("\n______________________________________________________");
+    printf("\ntotal\t\t\t\t\t:%f\t\t\tavg:%f",total,average);
+    getch();
+    return 0;
